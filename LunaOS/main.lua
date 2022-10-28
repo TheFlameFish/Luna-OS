b9a3476a11448da8f938043ce5e43a089da25913
Ash McCoy <mccoa499@gmail.com>
2022-10-27 20:41:22 -0700

    Added taskbar and changed background color.

--- LunaOS/main.lua
+++ LunaOS/main.lua
@@ -1,5 +1,11 @@
 local basalt = require("basalt")
 
 local main = basalt.createFrame("main")
+    :setBackground(colors.lightGray)
 
 main:show()
+
+taskbar = main:addMenubar()
+    :setSize(45, 1)
+    :setPosition(6,19)
+    :setBackground(colors.lightBlue)
