7431d7e97807e769f9b1f849ac507634fd303fe3
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2022-11-04 15:24:38 -0700

    Added some buttons to the taskbar

--- LunaOS/programs/main.lua
+++ LunaOS/programs/main.lua
@@ -1,16 +1,50 @@
+args = {...}
+local user = args[1]
+userDir = ("users/"..user)
+
 local basalt = require("../../basalt")
 
 local main = basalt.createFrame("main")
     :setBackground(colors.lightGray)
 
+local termX,termY = term.getSize()
 main:show()
 
+  -- create taskbar
 taskbar = main:addMenubar()
-    :setSize(45, 1)
-    :setPosition(6,19)
+    :setSize(termX - 6, 1)
+    :setPosition(7,termY)
     :setBackground(colors.lightBlue)
+    :setScrollable(true)
+    :setSpace(3)
 home = main:addButton()
-    :setPosition(1,19)
+    :setPosition(1,termY)
     :setSize(6, 1)
     :setText("Home")
     :setBackground(colors.blue)
+
+function fillTaskbar()
+    pinnedFile = fs.open(userDir.."/userSettings/pinnedFiles.txt","r")
+   
+    local pinned = {}
+    while true do
+        local line = pinnedFile.readLine()
+        pinnedFile.readLine()
+      
+        -- If line is nil then we've reached the end of the file and should stop
+        if not line then break end
+      
+        pinned[#pinned + 1] = line
+
+        
+    end
+    taskbar:addItem("Term", colors.black, colors.green)
+    for i, o in ipairs(pinned) do
+        taskbar:addItem(o,colors.green)
+    end
+    
+
+
+end
+fillTaskbar()
+basalt.autoUpdate()
