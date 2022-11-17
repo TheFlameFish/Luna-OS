f6aa52cdd1fba9ae284d905ae283c5f1f531f7a9
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2022-11-17 12:18:37 -0800

    Started work on install script.

---
+++ install.lua
@@ -0,0 +1,44 @@
+-- Make directories
+print("creating directories...")
+os.sleep(1)
+print("LunaOS/programs")
+fs.makeDir("LunaOS/programs")
+print("LunaOS/adminOnly")
+fs.makeDir("LunaOS/adminOnly")
+print("LunaOS/Sattelyte")
+print("LunaOS/Sattelyte/local")
+fs.makeDir("LunaOS/Sattelyte/local")
+print("LunaOS/Sattelyte/local/Cache")
+fs.makeDir("LunaOS/Sattelyte/local/Cache")
+print("LunaOS/Sattelyte/local/home")
+fs.makeDir("LunaOS/Sattelyte/local/home")
+print("users")
+fs.makeDir("users")
+os.sleep(2)
+
+-- create some files
+print("Creating files...")
+os.sleep(3)
+
+local nextId = fs.open("users/nextId.txt","w")
+nextId.writeLine("0")
+nextId.close()
+print("nextId.txt")
+
+local cachedSites = fs.open("LunaOS/Sattelyte/local/Cache/cachedSites.txt", "w")
+cachedSites.close()
+print("cachedSites.txt")
+
+local visitedSites = fs.open("LunaOS/Sattelyte/local/Cache/chachedSites.txt", "w")
+visitedSites.close()
+print("visitedSites.txt")
+os.sleep(1)
+-- Grab other files from Github
+print("Getting remaining files from Github...")
+os.sleep(3)
+
+local mainRequest = http.get("https://raw.githubusercontent.com/TheFlameFish/Luna-OS/Dev/LunaOS/programs/main.lua")
+
+local mainFile = fs.open("LunaOS/programs/main.lua", "w")
+fs.write(mainRequest.readAll())
+mainRequest.close()
