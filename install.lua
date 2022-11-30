e13654e8368004604ed48777afb7e01c08c77da4
TheFlameFish <114826427+TheFlameFish@users.noreply.github.com>
2022-11-30 09:41:24 -0800

    e

--- install.lua
+++ install.lua
@@ -41,7 +41,9 @@ local mainRequest = http.get("https://raw.githubusercontent.com/TheFlameFish/Lun
 
 local mainFile = fs.open("LunaOS/programs/main.lua", "w")
 
-for i,o in ipairs(mainRequest:readAll()) do
+local main = mainRequest:readAll()
+
+for i,o in ipairs(main) do
     mainFile.writeLine(o)
     print(o)
     os.sleep(0.1)
